ef3ea74c8c303c53e98244e0496880937fb26522
Antonio Garcés <garces080995@gmail.com>
2024-01-29 13:04:34 +0100

    Refactor: Message is a DTO, move the capability to send or reply messages to Node

--- src/echo/message.rs
+++ src/echo/message.rs
@@ -10,74 +10,11 @@ pub struct Message {
 }
 
 impl Message {
-    pub fn new(src: &str, dest: &str, body: Body) -> Self {
+    pub fn new(src: &str, dest: &str, body: &Body) -> Self {
         Self {
             src: src.to_owned(),
             dest: dest.to_owned(),
-            body,
+            body: body.to_owned(),
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use serde_json::json;
-
-    use crate::echo::payload::Payload;
-
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let req = json! {
-            {
-              "src": "c1",
-              "dest": "n1",
-              "body": {
-                "msg_id": 1,
-                "type": "echo",
-                "echo": "Please echo 35"
-              }
-            }
-        };
-        let res = json! {
-            {
-              "src": "n1",
-              "dest": "c1",
-              "body": {
-                "msg_id": 1,
-                "in_reply_to": 1,
-                "type": "echo_ok",
-                "echo": "Please echo 35"
-              }
-            }
-        };
-        let serde_req = serde_json::from_value::<Message>(req).unwrap();
-        let serde_res = serde_json::from_value::<Message>(res).unwrap();
-
-        let msg_req = Message::new(
-            "c1",
-            "n1",
-            Body::new(
-                Some(1),
-                None,
-                Payload::Echo {
-                    echo: "Please echo 35".to_string(),
-                },
-            ),
-        );
-        let msg_res = Message::new(
-            "n1",
-            "c1",
-            Body::new(
-                Some(1),
-                Some(1),
-                Payload::EchoOk {
-                    echo: "Please echo 35".to_string(),
-                },
-            ),
-        );
-        assert_eq!(serde_req, msg_req);
-        assert_eq!(serde_res, msg_res);
     }
 }
--- src/echo/node.rs
+++ src/echo/node.rs
@@ -19,6 +19,8 @@ impl Node {
                 node_ids: _ids,
             } => Payload::InitOk {},
             Payload::InitOk {} => Payload::InitOk {},
+            Payload::Generate {} => Payload::GenerateOk {},
+            Payload::GenerateOk {} => Payload::GenerateOk {},
         };
         Message {
             src: req.dest.clone(),
@@ -34,5 +36,89 @@ impl Node {
         serde_json::to_writer(&mut channel, &res)?;
         channel.write_all(&[b'\n'])?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_makes_an_echo_ok_resp() {
+        let body = Body::new(
+            Some(1),
+            None,
+            Payload::Echo {
+                echo: "Please echo 35".to_owned(),
+            },
+        );
+        let req = Message::new("c1", "n1", &body);
+        let res = Message::new(
+            "n1",
+            "c1",
+            &Body {
+                in_reply_to: body.msg_id,
+                payload: Payload::EchoOk {
+                    echo: "Please echo 35".to_owned(),
+                },
+                ..body
+            },
+        );
+
+        let node = Node;
+
+        assert_eq!(res, node.reply(req));
+    }
+    #[test]
+    fn it_makes_an_init_ok_resp() {
+        let body = Body::new(
+            Some(1),
+            None,
+            Payload::Init {
+                node_id: "n3".to_string(),
+                node_ids: vec!["n1".to_string(), "n2".to_string(), "n3".to_string()],
+            },
+        );
+        let req = Message::new("c1", "n1", &body);
+        let res = Message::new(
+            "n1",
+            "c1",
+            &Body {
+                in_reply_to: body.msg_id,
+                payload: Payload::InitOk {},
+                ..body
+            },
+        );
+
+        let node = Node;
+
+        assert_eq!(res, node.reply(req));
+    }
+    #[test]
+    fn it_should_write_resp_into_channel_provided() {
+        let body = Body::new(
+            Some(1),
+            None,
+            Payload::Echo {
+                echo: "Please echo 35".to_owned(),
+            },
+        );
+        // Make Message to send into "Channel"
+        let req = Message::new("c1", "n1", &body);
+        // Response should be JSON in bytes
+        let mut response = serde_json::to_string(&req).unwrap().into_bytes();
+        // Add a newline at the end
+        response.push(b'\n');
+
+        // Make a node that send messages
+        let node = Node;
+        // As send() accept a Writer, use a Cursor to mock an in-memory buffer that is cheaper than other Writer
+        let mut channel = std::io::Cursor::new(Vec::new());
+        // Send the message into buffer
+        let result = node.send(req, &mut channel);
+
+        assert!(result.is_ok());
+        // Channel should have the JSON as bytes + a newline.
+        assert_eq!(channel.get_ref().to_owned(), response);
     }
 }

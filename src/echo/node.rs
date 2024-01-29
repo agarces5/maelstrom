84b8c84a818f5e21111ed438d77fb99e231692d3
Antonio Garcés <garces080995@gmail.com>
2024-01-29 14:45:40 +0100

    Feat: Expand bussiness logic
    - Generate and GenerateOK new messages.
    - Add 'node_id' field to tag one node.
        node_id is set when receive an 'Init' message.
    - Add 'id' field to generate an unique_id.
        id increment each 'Generate' message.
    - Fix test broken by add new fields.
    - New test: Generate differents ids.

--- src/echo/node.rs
+++ src/echo/node.rs
@@ -3,10 +3,13 @@ use serde::{Deserialize, Serialize};
 use super::{body::Body, message::Message, payload::Payload};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Node;
+pub struct Node {
+    pub node_id: String,
+    pub id: usize,
+}
 
 impl Node {
-    pub fn reply(&self, req: Message) -> Message {
+    pub fn reply(&mut self, req: Message) -> Message {
         let payload = match &req.body.payload {
             Payload::Echo { echo } => Payload::EchoOk {
                 echo: echo.to_string(),
@@ -15,12 +18,20 @@ impl Node {
                 echo: echo.to_string(),
             },
             Payload::Init {
-                node_id: _id,
+                node_id,
                 node_ids: _ids,
-            } => Payload::InitOk {},
+            } => {
+                self.node_id = node_id.to_owned();
+                Payload::InitOk {}
+            }
             Payload::InitOk {} => Payload::InitOk {},
-            Payload::Generate {} => Payload::GenerateOk {},
-            Payload::GenerateOk {} => Payload::GenerateOk {},
+            Payload::Generate {} => {
+                self.id += 1;
+                Payload::GenerateOk {
+                    id: format!("{}-{}", self.node_id, self.id),
+                }
+            }
+            Payload::GenerateOk { id } => Payload::GenerateOk { id: id.to_string() },
         };
         Message {
             src: req.dest.clone(),
@@ -65,7 +76,10 @@ mod test {
             },
         );
 
-        let node = Node;
+        let mut node = Node {
+            node_id: String::from("n1"),
+            id: 0,
+        };
 
         assert_eq!(res, node.reply(req));
     }
@@ -90,7 +104,10 @@ mod test {
             },
         );
 
-        let node = Node;
+        let mut node = Node {
+            node_id: String::from("n1"),
+            id: 0,
+        };
 
         assert_eq!(res, node.reply(req));
     }
@@ -111,7 +128,10 @@ mod test {
         response.push(b'\n');
 
         // Make a node that send messages
-        let node = Node;
+        let node = Node {
+            node_id: String::from("n1"),
+            id: 0,
+        };
         // As send() accept a Writer, use a Cursor to mock an in-memory buffer that is cheaper than other Writer
         let mut channel = std::io::Cursor::new(Vec::new());
         // Send the message into buffer
@@ -120,5 +140,28 @@ mod test {
         assert!(result.is_ok());
         // Channel should have the JSON as bytes + a newline.
         assert_eq!(channel.get_ref().to_owned(), response);
+    }
+    #[test]
+    fn it_should_generate_differents_id() {
+        let body = Body {
+            payload: Payload::Generate {},
+            ..Default::default()
+        };
+
+        let req = Message::new("c1", "n1", &body);
+        let ids = Vec::from_iter((1..6).map(|n| format!("n1-{}", n)));
+
+        let mut node = Node {
+            node_id: String::from("n1"),
+            id: 0,
+        };
+
+        let generated_ids: Vec<String> = (0..ids.len())
+            .map(|_| match node.reply(req.clone()).body.payload {
+                Payload::GenerateOk { id } => id,
+                _ => String::new(),
+            })
+            .collect();
+        assert_eq!(ids, generated_ids);
     }
 }

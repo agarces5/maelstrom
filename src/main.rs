ac574d4f8e456421663b8b91fb636160dce72594
Antonio Garcés <garces080995@gmail.com>
2024-02-01 07:34:42 +0100

    Refactor: Swap stdin to use lines instead of give stdin to serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use maelstrom::echo::message::Message;
+use std::io::BufRead;
+
 use maelstrom::echo::node::Node;
 
 fn main() -> anyhow::Result<()> {
+    // let (tx, rx) = std::sync::mpsc::channel();
+
     let mut node = Node {
         id: 0,
         node_id: String::new(),
         messages: Vec::new(),
     };
-    let stdin = std::io::stdin().lock();
+    let stdin = std::io::stdin().lock().lines();
     let mut stdout = std::io::stdout().lock();
 
-    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
-    for msg in inputs {
-        let res = node.reply(msg?);
+    for msg in stdin {
+        let msg = serde_json::from_str(&msg?)?;
+        let res = node.reply(msg);
         node.send(res, &mut stdout)?;
     }
     Ok(())

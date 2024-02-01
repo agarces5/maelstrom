9687fcdaea998f5ba1734449359e3deb8dc19525
Antonio Garcés <garces080995@gmail.com>
2024-02-01 08:36:11 +0100

    Refactor: Split sender in a new thread

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use std::io::BufRead;
+use std::{
+    io::BufRead,
+    sync::{Arc, Mutex},
+};
 
 use maelstrom::echo::node::Node;
 
 fn main() -> anyhow::Result<()> {
-    // let (tx, rx) = std::sync::mpsc::channel();
+    let (tx, rx) = std::sync::mpsc::channel();
 
-    let mut node = Node {
+    let node = Arc::new(Mutex::new(Node {
         id: 0,
         node_id: String::new(),
         messages: Vec::new(),
-    };
-    let stdin = std::io::stdin().lock().lines();
+    }));
     let mut stdout = std::io::stdout().lock();
 
-    for msg in stdin {
-        let msg = serde_json::from_str(&msg?)?;
-        let res = node.reply(msg);
-        node.send(res, &mut stdout)?;
+    let resp_node = node.clone();
+    let resp_thread = std::thread::spawn(move || {
+        let node = resp_node;
+        let stdin = std::io::stdin().lock().lines();
+        for msg in stdin {
+            let msg = serde_json::from_str(&msg?)?;
+            let res = node.lock().unwrap().reply(msg);
+            eprintln!("Message received: {:?}", res);
+            tx.send(res)?;
+            // node.send(res, &mut stdout)?;
+        }
+        anyhow::Ok(())
+    });
+    while let Ok(res) = rx.recv() {
+        eprintln!("{:?}", res);
+        node.lock().unwrap().send(res, &mut stdout)?;
     }
+    resp_thread
+        .join()
+        .expect("Can't join response thread")
+        .expect("Some error inside resp thread");
     Ok(())
 }
